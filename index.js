ce737aaba894272f4989490d55bea563c1a5b8a4
LilianMathu <lilianmathu2018@gmail.com>
2019-07-27 11:40:42 +0300

    create two arrays to be used in the project

---
+++ index.js
@@ -0,0 +1,15 @@
+const people = [
+    {name: "lilian", age:22, married: false, location: "Kisumu "},
+    {name: "Eleanor", age:25, married: true, location: "Ngong "},
+    {name: "Nyakio", age:21, married: true, location: "KIserian "},
+    {name: "Liz", age:49, married: true, location: "Gategi "},
+    {name: "Mathu", age:55, married: true, location: "Kiambu "},
+    {name: "Tonny", age: 31, married: false, location: "Twala "},
+];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// foreach. It is a better way to loop through arrays
+// filter 
+// sort
+// reduce 
+// map 
