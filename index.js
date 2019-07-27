1bcea422f36b9416f5a4a4b292f83ece36e353bd
LilianMathu <lilianmathu2018@gmail.com>
2019-07-27 12:06:07 +0300

    forEach

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ const people = [
 ];
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// foreach. It is a better way to loop through arrays
+/* forEach
+  It is a better way to loop through arrays. It does not return anything
+
+ standard for loop to compare with the forEach
+ for (let i = 0; i < people.length; i++) {
+      console.log(people[i]);   
+ }
+*/
+
+people.forEach((person)=> {
+    console.log(person.name);
+})
+number.forEach(function(index){
+    console.log(index);
+})
+
 // filter 
 // sort
 // reduce 
